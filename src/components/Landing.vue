5336a934e73eebc8006bdda5d18ca85f035ce4d3
SunnyL17 <cliu@stcloudstate.edu>
2017-11-27 11:47:41 -0600

    First commit

---
+++ src/components/Landing.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <p>Hey there! {{user.email}}, check out our new features.</p>   
+        <v-wid></v-wid>
+    </div>
+</template>
+<script>
+import Widget from './Widget'
+export default{
+    name:'Landing',
+    components:
+    {
+    'v-wid':Widget
+        },
+    computed:{
+        user(){
+            return this.$store.state.user
+        }
+    },
+    methods:{
+        computed:{
+        user(){
+            return this.$store.state.user
+            
+        }
+    }
+        
+        
+    /*,
+    GoBack(){
+       this.$store.commit('ShareInfo',{
+              input1:this.user.input1
+              
+          }); 
+          this.$router.push({path:'/'})      
+    }
+    */
+   }
+}
+</script>
+<style scoped>
+ header
+{
+  color: crimson;
+  width: 100%;
+  height: 20%;
+    }
+.title{
+    color:#9575CD;
+    font-weight: bold;
+    font-size: 200%;
+
+    }
+</style>
